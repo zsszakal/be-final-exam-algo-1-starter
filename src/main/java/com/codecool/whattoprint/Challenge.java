5450df17301d03c55cd50a553bdd4725a5c0ebb2
zsszakal <zs.szakal@outlook.com>
2021-03-24 12:01:10 +0100

    Implement method in Challenge.java

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,14 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        String result ="";
+
+        if (number % 3 == 0){
+            result += words[0];
+        }
+        if (number % 7 == 0){
+            result += words[1];
+        }
+        return result;
     }
 }
